703252dfa77a4692f05a6e20d8c329f841ac501d
Kevin Jiang <68627312+kevjiang23@users.noreply.github.com>
2021-12-06 16:37:52 -0800

    Add files via upload

---
+++ DataLibrary/BusinessLogic/BundleProcessor.cs
@@ -0,0 +1,34 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class BundleProcessor
+    {
+        public static int CreateBundle(int itemID, string itemName, int quantity)
+        {
+            BundleModel data = new BundleModel
+            {
+                ItemID = itemID,
+                ItemName = itemName,
+                Quantity = quantity
+            };
+
+            string sql = @"insert into dbo.Bundle (ItemID, ItemName, Quantity) 
+                        values (@ItemID, @ItemName, @Quantity);";
+
+            return SqlDataAccess.SaveData(sql, data);
+        }
+        public static List<BundleModel> LoadBundles()
+        {
+            string sql = @"select Id, ItemID, ItemName, Quantity
+                        from dbo.Bundle;";
+            return SqlDataAccess.LoadData<BundleModel>(sql);
+        }
+    }
+}
---
+++ DataLibrary/Models/BundleModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.Models
+{
+    public class BundleModel
+    {
+        public int Id { get; set; }
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
